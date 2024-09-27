10f8e54bd10c88124e787cec59651ec8415f98f6
Jesus Fernando Garzon Garcia <81579165+JesusFernandoGarzonGarcia@users.noreply.github.com>
2024-09-26 19:22:54 -0500

    correccio de metodos duplicados

--- src/main/java/co/edu/uptc/animals_rest/models/Animal.java
+++ src/main/java/co/edu/uptc/animals_rest/models/Animal.java
@@ -2,18 +2,20 @@ package co.edu.uptc.animals_rest.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor // Default no-args constructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class Animal {
     private String name;
     private String category;
     private String numberByCategory;
 
-     public Animal(String name, String category) {
+    // Constructor with only two parameters (name, category)
+    public Animal(String name, String category) {
         this.name = name;
         this.category = category;
     }
